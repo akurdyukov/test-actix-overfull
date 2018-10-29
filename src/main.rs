44bae1b660d9d497d29857392630c1ac9f23d47f
Alik Kurdyukov <akurdyukov@gmail.com>
2018-10-29 17:09:19 +0300

    simplification

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ extern crate tokio;
 extern crate log;
 extern crate env_logger;
 
-use std::collections::HashMap;
 use actix::prelude::*;
 
 #[derive(Debug)]
@@ -28,49 +27,13 @@ impl Command {
     }
 }
 
-struct LocalActor {
-    pub user_id: u64,
-}
-
-impl LocalActor {
-    fn new(user_id: u64) -> Self {
-        LocalActor {
-            user_id,
-        }
-    }
-}
-
-impl Actor for LocalActor {
-    type Context = Context<Self>;
-
-    fn started(&mut self, _: &mut Self::Context) {
-        info!("LocalActor for {:?} started", self.user_id);
-    }
-}
-
-impl Handler<Command> for LocalActor {
-    type Result = ();
-
-    fn handle(&mut self, msg: Command, _: &mut Context<Self>) -> Self::Result {
-        info!("Executing command {:?}!", msg);
-    }
-}
-
-
 struct LocalSupervisor {
-    user_to_address: HashMap<u64, Addr<LocalActor>>
 }
 
 impl LocalSupervisor {
     pub fn new() -> Self {
         LocalSupervisor {
-            user_to_address: HashMap::new()
         }
-    }
-
-    fn get_child_addr(&mut self, user_id: u64) -> &Addr<LocalActor> {
-        self.user_to_address.entry(user_id.clone())
-            .or_insert_with(|| LocalActor::new(user_id).start() )
     }
 }
 
@@ -93,9 +56,6 @@ impl Handler<Command> for LocalSupervisor {
 
     fn handle(&mut self, msg: Command, _: &mut Context<Self>) -> Self::Result {
         info!("Supervisor accepted validating command {:?}", msg);
-
-        let addr = self.get_child_addr(msg.user_id);
-        addr.do_send(msg);
     }
 }
 
